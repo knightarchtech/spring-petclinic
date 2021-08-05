545dadbaebc2c0c64fe3df5a578d4acb8792e3ff
dev <info@knightarchtech.com>
2021-08-05 02:24:18 -0400

    Updated clean script

--- clean.sh
+++ clean.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-kubectl delete pipeline spring-petclinic
-kubectl delete imagestream spring-petclinic
-kubectl delete secret spring-petclinic-generic-webhook-secret
-kubectl delete secret spring-petclinic-github-webhook-secret
-kubectl delete deploymentconfig spring-petclinic
-kubectl delete buildconfig spring-petclinic
-kubectl delete service spring-petclinic
-kubectl delete route spring-petclinic
+kubectl delete pipeline spring-petclinic-test
+kubectl delete imagestream spring-petclinic-test
+kubectl delete secret spring-petclinic-test-generic-webhook-secret
+kubectl delete secret spring-petclinic-test-github-webhook-secret
+kubectl delete deployment spring-petclinic-test
+kubectl delete deploymentconfig spring-petclinic-test
+kubectl delete buildconfig spring-petclinic-test
+kubectl delete service spring-petclinic-test
+kubectl delete route spring-petclinic-test
 
 
